ca1348c50659d5c2eef48de9cb179942e3b105c4
Kiffin Gish <kiffin.gish@planet.nl>
2019-09-21 16:06:39 +0200

    Added class-transformer and class-validator for typeorm.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [AppController],
   providers: [AppService]
 })
